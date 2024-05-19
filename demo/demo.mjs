e32938bb82e922077fc9f094ffc03420037febf4
clasen <martin.clasen@gmail.com>
2024-05-19 15:38:18 -0300

    Update demo.mjs

--- demo/demo.mjs
+++ demo/demo.mjs
@@ -29,15 +29,18 @@ driver.attach(new CustomModel({
     }
 }));
 
+console.log("\n" + '--------| gpt-4o |--------');
+const gpt = await driver.create('gpt-4o', { temperature: 0.5 });
+const question = await gpt.addText("Have you ever eaten a cat?").message();
+console.log(question);
+
+console.log('--------| claude-3-sonnet-20240229 |--------');
 const claude = await driver.create('claude-3-sonnet-20240229', { temperature: 0.5 });
 await claude.addImage("./watson.png")
 const imageDescription = await claude.addText("describe the image").message();
 console.log(imageDescription);
 
-const gpt = await driver.create('gpt-4o', { temperature: 0.5 });
-const question = await gpt.addText("Have you ever eaten a cat?").message();
-console.log(question);
-
+console.log("\n" + '--------| pplx-70b-online |--------');
 const pplx = await driver.create('pplx-70b-online', { max_tokens: 500 });
 await pplx.addText('How much is ETH trading in USD?');
 const news = await pplx.addText('What are the 3 most recent Ethereum news?').message();
